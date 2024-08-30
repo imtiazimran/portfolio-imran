e28459c799862f1878f580995f21bec5a29a011d
imtiazimran <imtiazimran@outlook.com>
2024-08-30 21:42:22 +0600

    base changed

--- src/utils/BaseApi.ts
+++ src/utils/BaseApi.ts
@@ -1,2 +1,3 @@
-export const BaseApi = "https://portfolio-backend-rho-black.vercel.app/api/v1"
+// export const BaseApi = "https://portfolio-backend-rho-black.vercel.app/api/v1"
+export const BaseApi = "https://portfolio-backend-imrans-projects-fdf62f1a.vercel.app/api/v1"
 // export const BaseApi = "http://localhost:5000/api/v1"
